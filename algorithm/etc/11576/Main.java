fca3148931e53d5d4ff94492fc3833d7f4a6f863
yongsungjeon <specialys@gmail.com>
2018-10-10 10:33:51 +0900

    Base Conversion 문제 풀이 추가

--- algorithm/etc/11576/Main.java
+++ algorithm/etc/11576/Main.java
@@ -1,6 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
+
+/**
+ * Recursion
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,14 +22,43 @@ public class Main {
         }
 
         convert(answer, after);
-        System.out.println();
     }
 
-    public static void convert(int num, int base) {
+    private static void convert(int num, int base) {
         if (num == 0) {
             return;
         }
         convert(num / base, base);
         System.out.print(num % base + " ");
+    }
+}
+
+/**
+ * Stack
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        String[] arr = line.split(" ");
+        int before = Integer.parseInt(arr[0]);
+        int after = Integer.parseInt(arr[1]);
+        int num = Integer.parseInt(br.readLine());
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        arr = br.readLine().split(" ");
+
+        for (int i = 0; i < num; i++) {
+            answer = answer * before + Integer.parseInt(arr[i]);
+        }
+
+        while (answer != 0) {
+            stack.push(answer % after);
+            answer /= after;
+        }
+
+        while(!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
     }
 }
